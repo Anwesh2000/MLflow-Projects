089f5103f69fa90affb0a60b498eadcc192f6b22
Anwesh Nayak <39364980+Anwesh2000@users.noreply.github.com>
2021-07-19 19:49:02 +0530

    Update example.py

--- example.py
+++ example.py
@@ -14,7 +14,6 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random
 
 neighbors = int(sys.argv[1]) if len(sys.argv) > 1 else 2
 if __name__ == "__main__":
-    mlflow.set_experiment(experiment_name='KNeighbors')
     with mlflow.start_run(run_name=str(neighbors) + '_neighbors'):
         knn = KNeighborsClassifier(n_neighbors=neighbors)
         knn.fit(X_train, y_train)
