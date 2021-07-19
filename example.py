045026755f015802c99679857f3ca52c77f7aea8
Anwesh Nayak <39364980+Anwesh2000@users.noreply.github.com>
2021-07-19 20:21:28 +0530

    Update example.py

--- example.py
+++ example.py
@@ -6,7 +6,6 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
 
-mlflow.sklearn.autolog()
 data = load_iris()
 X = data.data
 y = data.target
@@ -17,3 +16,12 @@ if __name__ == "__main__":
     with mlflow.start_run(run_name=str(neighbors) + '_neighbors'):
         knn = KNeighborsClassifier(n_neighbors=neighbors)
         knn.fit(X_train, y_train)
+        y_pred = knn.predict(X_test)
+        accuracy_train = metrics.accuracy_score(y_train,knn.predict(X_train))
+        accuracy_test = metrics.accuracy_score(y_test,y_pred)
+        print("Number of neighbours : %s" % neighbors)
+        print("    train accuracy : %s" % accuracy_train)
+        print("    test accuracy  : %s" %accuracy_test)
+        mlflow.log_param("number_of_neighbors",neighbors)
+        mlflow.log_metric("train_accuracy", accuracy_train)
+        mlflow.log_metric("test_accuracy",accuracy_test)
